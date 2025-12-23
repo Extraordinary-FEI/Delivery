dd92e808fb11fcb45263ba38f7828a63a815026a
Extraordinary-FEI <3426417265@qq.com>
2025-12-23 14:43:26 +0800

    init

---
+++ app/src/testDebug/aidl/com/example/cn/helloworld/ui/admin/AdminOrderListActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.cn.helloworld.ui.admin;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by 34264 on 2025/11/26.
+ */
+public class AdminOrderListActivityTest extends TestCase {
+
+}
