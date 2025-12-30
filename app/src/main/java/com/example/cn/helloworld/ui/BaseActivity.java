bf6dbdb27050c12a03152847fe871aecfe80ad35
Extraordinary-FEI <3426417265@qq.com>
2025-12-30 14:48:36 +0800

    update master

--- app/src/main/java/com/example/cn/helloworld/ui/BaseActivity.java
+++ app/src/main/java/com/example/cn/helloworld/ui/BaseActivity.java
@@ -1,11 +1,19 @@
 package com.example.cn.helloworld.ui;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.cn.helloworld.utils.LocaleManager;
+import com.example.cn.helloworld.utils.ThemeManager;
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ThemeManager.apply(this);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     protected void attachBaseContext(Context newBase) {
